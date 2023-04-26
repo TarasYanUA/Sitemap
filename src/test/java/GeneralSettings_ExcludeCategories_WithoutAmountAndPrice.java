b128cf07cced582047752e0c4fdfc2639d44cd43
TarasYanUA <wildmonach@gmail.com>
2023-04-26 12:24:50 +0300

    First test-case is totally ready where everything is done automatically!

--- src/test/java/GeneralSettings_ExcludeCategories_WithoutAmountAndPrice.java
+++ src/test/java/GeneralSettings_ExcludeCategories_WithoutAmountAndPrice.java
@@ -3,8 +3,8 @@ import adminPanel.SitemapSettings;
 import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.Selenide;
 import com.codeborne.selenide.WebDriverRunner;
-import org.testng.Assert;
 import org.testng.annotations.Test;
+import org.testng.asserts.SoftAssert;
 import static com.codeborne.selenide.Selenide.*;
 
 /*
@@ -12,7 +12,7 @@ import static com.codeborne.selenide.Selenide.*;
     * Категория "iPods" - с ценой и в наличии
     * Категория "Android" - без цены и без наличия
 Настройка модуля: "Общие -- Исключить категории -- с товарами без цен и наличия"
-Итог:
+Проверяем, что:
     * Категория "iPods" присутствует в карте сайта
     * Категория "Android" отсутствует в карте сайта.
 */
@@ -25,7 +25,7 @@ public class GeneralSettings_ExcludeCategories_WithoutAmountAndPrice extends Tes
         csCartSettings.navigateToEditingCategoryPage();
         csCartSettings.selectCategory_Ipods.click();
         csCartSettings.goToStorefront_CategoryPage(1);
-        String currentUrl_CategoryIpods = WebDriverRunner.getWebDriver().getCurrentUrl(); //Запоминаем ссылку категории "iPods"
+        String currentUrl_CategoryIpods = WebDriverRunner.getWebDriver().getCurrentUrl(); //Получили ссылку категории "iPods"
         String[] arrayIpods = currentUrl_CategoryIpods.split("\\?");
         String urlForCategoryIpods = arrayIpods[0];
         System.out.println("iPods URL is: " + urlForCategoryIpods);
@@ -40,7 +40,7 @@ public class GeneralSettings_ExcludeCategories_WithoutAmountAndPrice extends Tes
         csCartSettings.navigateToEditingCategoryPage();
         csCartSettings.selectCategory_Android.click();
         csCartSettings.goToStorefront_CategoryPage(2);
-        String currentUrl_CategoryAndroid = WebDriverRunner.getWebDriver().getCurrentUrl(); //Запоминаем ссылку категории "Android"
+        String currentUrl_CategoryAndroid = WebDriverRunner.getWebDriver().getCurrentUrl(); //Получили ссылку категории "Android"
         switchTo().window(0);
         String[] arrayAndroid = currentUrl_CategoryAndroid.split("\\?");
         String urlForCategoryAndroid = arrayAndroid[0];
@@ -69,13 +69,30 @@ public class GeneralSettings_ExcludeCategories_WithoutAmountAndPrice extends Tes
         //Работаем с выгрузкой
         csCartSettings.navigateToSitemapGenerating();
         sitemapSettings.clickButton_GenerateSitemap();
-        String url = "https://trs.test.abt.team/4162mven/categories1.xml";
-        Selenide.executeJavaScript("window.open('"+url+"');");
+        $("a[href*='sitemap.xml']").click();
         csCartSettings.shiftBrowserTab(1);
+        String allUrls = $(".pretty-print").getText();
+        String[] splitOne = allUrls.split("<loc>");
+        int i=0;
+        while (i < splitOne.length) {
+            i++;    }
+        String preliminaryResult = splitOne[2];
+        String[] finalResult = preliminaryResult.split("</loc>");
+        int k=0;
+        while (k < finalResult.length) {
+            k++;    }
+        System.out.println("Final result is:" + finalResult[0]);
+        String urlForCategories = finalResult[0];   //Получили ссылку на карту сайта категорий
+        Selenide.executeJavaScript("window.open('"+urlForCategories+"');");
+        csCartSettings.shiftBrowserTab(2);
         //Проверяем, что ссылка на категорию "iPods" присутствует
-        Assert.assertTrue($(".pretty-print").has(Condition.text(urlForCategoryIpods)));
+        SoftAssert softAssert = new SoftAssert();
+        softAssert.assertTrue($(".pretty-print").has(Condition.text(urlForCategoryIpods)),
+                "There is no link for category 'iPods'!");
         //Проверяем, что ссылка на категорию "Android" отсутствует
-        Assert.assertFalse($(".pretty-print").has(Condition.text(urlForCategoryAndroid)));
+        softAssert.assertFalse($(".pretty-print").has(Condition.text(urlForCategoryAndroid)),
+                "There is a link for category 'Android' but shouldn't!");
         screenshot("GeneralSettings_ExcludeCategories_WithoutAmountAndPrice");
+        softAssert.assertAll();
     }
 }
