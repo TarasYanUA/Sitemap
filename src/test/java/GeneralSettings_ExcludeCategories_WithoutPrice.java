3b94cae2a71950cd3ee3971fdbf559a76a7890ba
TarasYanUA <wildmonach@gmail.com>
2023-04-27 15:17:04 +0300

    Four test-cases are ready! Added TestNG file.

---
+++ src/test/java/GeneralSettings_ExcludeCategories_WithoutPrice.java
@@ -0,0 +1,100 @@
+import adminPanel.CsCartSettings;
+import adminPanel.SitemapSettings;
+import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.Selenide;
+import com.codeborne.selenide.WebDriverRunner;
+import org.testng.annotations.Test;
+import org.testng.asserts.SoftAssert;
+
+import static com.codeborne.selenide.Selenide.*;
+
+/*
+Двум категориям "iPods" и "Android" настраиваем по 2 товара:
+    * Категория "iPods" - с ценой и в наличии
+    * Категория "Android" - без цен и с наличием
+Настройка модуля: "Общие -- Исключить категории -- с товарами без цен"
+Проверяем, что:
+    * Категория "iPods" присутствует в карте сайта
+    * Категория "Android" отсутствует в карте сайта
+*/
+
+public class GeneralSettings_ExcludeCategories_WithoutPrice extends TestRunner{
+    @Test()
+    public void checkGeneralSettings_ExcludeCategories_WithoutPrice() {
+        CsCartSettings csCartSettings = new CsCartSettings();
+        //Настраиваем первую категорию "iPods"
+        csCartSettings.navigateToEditingCategoryPage();
+        csCartSettings.selectCategory_Ipods.click();
+        csCartSettings.goToStorefront_CategoryPage(1);
+        String currentUrl_CategoryIpods = WebDriverRunner.getWebDriver().getCurrentUrl(); //Получили ссылку категории "iPods"
+        String[] arrayIpods = currentUrl_CategoryIpods.split("\\?");
+        String urlForCategoryIpods = arrayIpods[0];
+        System.out.println("iPods URL is: " + urlForCategoryIpods);
+        csCartSettings.shiftBrowserTab(0);
+        csCartSettings.navigateToProductsOfCategory();
+        csCartSettings.clickAndType_PriceForCategory_ProdOne("249.00");
+        csCartSettings.clickAndType_AmountForCategory_ProdOne("10");
+        csCartSettings.clickAndType_PriceForCategory_ProdTwo("255.00");
+        csCartSettings.clickAndType_AmountForCategory_ProdTwo("15");
+        csCartSettings.button_Save.click();
+        //Настраиваем вторую категорию "Android"
+        csCartSettings.navigateToEditingCategoryPage();
+        csCartSettings.selectCategory_Android.click();
+        csCartSettings.goToStorefront_CategoryPage(2);
+        String currentUrl_CategoryAndroid = WebDriverRunner.getWebDriver().getCurrentUrl(); //Получили ссылку категории "Android"
+        csCartSettings.shiftBrowserTab(0);
+        String[] arrayAndroid = currentUrl_CategoryAndroid.split("\\?");
+        String urlForCategoryAndroid = arrayAndroid[0];
+        System.out.println("Android URL is: " + urlForCategoryAndroid);
+        csCartSettings.navigateToProductsOfCategory();
+        if($$(".products-list__image").size() > 2){
+            csCartSettings.deleteProductsFromCategory();
+        }
+        csCartSettings.clickAndType_PriceForCategory_ProdOne("0.00");
+        csCartSettings.clickAndType_AmountForCategory_ProdOne("10");
+        csCartSettings.clickAndType_PriceForCategory_ProdTwo("0.00");
+        csCartSettings.clickAndType_AmountForCategory_ProdTwo("15");
+        csCartSettings.button_Save.click();
+
+        //Настраиваем настройки модуля
+        SitemapSettings sitemapSettings = csCartSettings.navigateToSitemapSettings();
+        sitemapSettings.tab_Settings.click();
+        sitemapSettings.setting_ExcludeCategories.selectOptionByValue("without_product_price");
+        sitemapSettings.tab_XMLSitemap.click();
+        if(!sitemapSettings.setting_EnableXMLSitemap.isSelected()){
+        sitemapSettings.setting_EnableXMLSitemap.click();   }
+        if(!sitemapSettings.setting_CategoriesSettings_IncludeToSitemap.isSelected()){
+            sitemapSettings.setting_CategoriesSettings_IncludeToSitemap.click();    }
+        csCartSettings.button_Save.click();
+
+        //Работаем с выгрузкой
+        csCartSettings.navigateToSitemapGenerating();
+        sitemapSettings.clickButton_GenerateSitemap();
+        $("a[href*='sitemap.xml']").click();
+        csCartSettings.shiftBrowserTab(3);
+        String allUrls = $(".pretty-print").getText();
+        String[] splitOne = allUrls.split("<loc>");
+        int i=0;
+        while (i < splitOne.length) {
+            i++;    }
+        String preliminaryResult = splitOne[2];
+        String[] finalResult = preliminaryResult.split("</loc>");
+        int k=0;
+        while (k < finalResult.length) {
+            k++;    }
+        System.out.println("Final result is: " + finalResult[0]);
+        String urlForCategories = finalResult[0];   //Получили ссылку на карту сайта категорий
+        Selenide.executeJavaScript("window.open('"+urlForCategories+"');");
+        csCartSettings.shiftBrowserTab(4);
+        //Проверяем, что ссылка на категорию "iPods" присутствует
+        SoftAssert softAssert = new SoftAssert();
+        softAssert.assertTrue($(".pretty-print").has(Condition.text(urlForCategoryIpods)),
+                "There is no link for category 'iPods'!");
+        //Проверяем, что ссылка на категорию "Android" отсутствует
+        softAssert.assertFalse($(".pretty-print").has(Condition.text(urlForCategoryAndroid)),
+                "There is a link for category 'Android' but shouldn't!");
+        screenshot("GeneralSettings_ExcludeCategories_WithoutPrice");
+        softAssert.assertAll();
+        System.out.println("GeneralSettings_ExcludeCategories_WithoutPrice has passed successfully!");
+    }
+}
