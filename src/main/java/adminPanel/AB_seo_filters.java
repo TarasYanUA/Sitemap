d92d94194e159b9d055ef54a0c775154dbc66acc
TarasYanUA <wildmonach@gmail.com>
2023-05-08 17:42:19 +0300

    Test-case Integration_AB_seo_filters is ready!

--- src/main/java/adminPanel/AB_seo_filters.java
+++ src/main/java/adminPanel/AB_seo_filters.java
@@ -1,11 +1,56 @@
 package adminPanel;
 
 import com.codeborne.selenide.SelenideElement;
-
 import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.$x;
 
 public class AB_seo_filters {
     public AB_seo_filters(){super();}
 
-    public SelenideElement setting_AddSeoPagesToSitemap = $("select[input*='addon_option_ab__seo_filters_add_to_sitemap_']");
+    public SelenideElement setting_AddSeoPagesToSitemap = $("select[id*='addon_option_ab__seo_filters_add_to_sitemap_']");
+    private SelenideElement ab_menu = $(".ab__am-menu .dropdown-toggle");
+    private SelenideElement generationRulesForFilters = $(".ab__am-menu a[href$='ab__sf_rules.manage']");
+    public SelenideElement button_AddRule = $x("//div[@id='tools_manage_ab__sf_rules_adv_buttons']/div");
+    public SelenideElement field_Features = $("ul.select2-selection__rendered");
+    public SelenideElement feature_OperatingSystem = $x("//div[contains(text(), 'Операционная система')]");
+    public SelenideElement feature_Brand = $x("//div[contains(text(), 'Бренд [18]')]");
+    public SelenideElement button_AddCategories = $x("//a[contains(@id, 'opener_picker_elm_categories')]");
+    public SelenideElement categoryComputers = $("#input_cat_167");
+    public SelenideElement button_SaveCategories = $(".cm-form-dialog-closer");
+    public SelenideElement checkbox_IncludeSubcategories = $("#elm_subcats");
+    public SelenideElement select_ParentCategories = $("#elm_generated_categories");
+    public SelenideElement placeholderOne_PageTagH1 = $("#elm_tag_h1");
+    public SelenideElement placeholderTwo_PageTitle = $("#elm_page_title");
+    public SelenideElement placeholderThree_PageMetaTagDescription = $("#elm_meta_description");
+    public SelenideElement placeholderFour_PageMetaTagKeywords = $("#elm_meta_keywords");
+    public SelenideElement placeholderFive_PageBreadcrumb = $("#elm_breadcrumb");
+    public SelenideElement placeholderSix_ProductPageBreadcrumb = $("#elm_product_breadcrumb");
+    public SelenideElement button_Create = $(".nav__actions-bar .cm-submit");
+    public SelenideElement gearwhealOnRulePage = $(".nav__actions-bar .dropdown-icon--tools");
+    public SelenideElement button_GenerateRulePage = $("a[href*='ab__sf_rules.generate']");
+    private SelenideElement section_SeoPagesList = $(".adv-buttons a[href$='ab__sf_names.manage']");
+
+
+    public void navigateToGenerationRulesForFilters(){
+        ab_menu.click();
+        generationRulesForFilters.click();
+    }
+    public void placeAllPlaceholdersOnRulePage(String placeholder){
+        placeholderOne_PageTagH1.click();
+        placeholderOne_PageTagH1.sendKeys("placeholder");
+        placeholderTwo_PageTitle.click();
+        placeholderTwo_PageTitle.sendKeys(placeholder);
+        placeholderThree_PageMetaTagDescription.click();
+        placeholderThree_PageMetaTagDescription.sendKeys(placeholder);
+        placeholderFour_PageMetaTagKeywords.click();
+        placeholderFour_PageMetaTagKeywords.sendKeys(placeholder);
+        placeholderFive_PageBreadcrumb.click();
+        placeholderFive_PageBreadcrumb.sendKeys(placeholder);
+        placeholderSix_ProductPageBreadcrumb.click();
+        placeholderSix_ProductPageBreadcrumb.sendKeys(placeholder);
+    }
+    public void navigateToSeoPagesList(){
+        ab_menu.click();
+        section_SeoPagesList.click();
+    }
 }
--- src/test/java/Integration_AB_seo_filters.java
+++ src/test/java/Integration_AB_seo_filters.java
@@ -1,13 +1,75 @@
 import adminPanel.AB_seo_filters;
 import adminPanel.CsCartSettings;
+import adminPanel.SitemapSettings;
+import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.Selenide;
+import com.codeborne.selenide.WebDriverRunner;
+import org.openqa.selenium.Alert;
 import org.testng.annotations.Test;
+import org.testng.asserts.SoftAssert;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.$$;
 
 public class Integration_AB_seo_filters extends TestRunner{
     @Test
     public void checkIntegration_AB_seo_filters(){
         CsCartSettings csCartSettings = new CsCartSettings();
-        //Устанавливаем модуль "AB: SEO страницы для фильтров"
+        //Работа с модулем "AB: SEO страницы для фильтров"
         csCartSettings.installAddonAtAddonsManager(csCartSettings.menuOfAB__seo_filters, "ab__seo_filters", "form[name=ab_install_form_54338]");
         AB_seo_filters ab_seo_filters = csCartSettings.navigateToGeneralSettingsOf_ab_seo_filters();
+        ab_seo_filters.setting_AddSeoPagesToSitemap.selectOptionByValue("all");
+        csCartSettings.button_Save.click();
+        ab_seo_filters.navigateToGenerationRulesForFilters();
+        ab_seo_filters.button_AddRule.click();
+        ab_seo_filters.field_Features.click();
+        ab_seo_filters.feature_OperatingSystem.click();
+        ab_seo_filters.feature_Brand.click();
+        ab_seo_filters.checkbox_IncludeSubcategories.click();
+        ab_seo_filters.button_AddCategories.click();
+        $(".ui-dialog").shouldBe(Condition.visible);
+        ab_seo_filters.categoryComputers.click();
+        ab_seo_filters.button_SaveCategories.click();
+        ab_seo_filters.select_ParentCategories.selectOptionByValue("by_all_filter_categories");
+        ab_seo_filters.placeAllPlaceholdersOnRulePage("[category] [variant]");
+        ab_seo_filters.button_Create.click();
+        ab_seo_filters.gearwhealOnRulePage.shouldBe(Condition.enabled).click();
+        ab_seo_filters.button_GenerateRulePage.click();
+        Alert alert = Selenide.webdriver().driver().switchTo().alert();
+        alert.accept();
+        Selenide.sleep(2000);
+        if($$(".cm-notification-close").size() > 0){
+            do{
+                $(".cm-notification-close").click();
+            } while ($$(".cm-notification-close").size() > 0);
+        }
+        ab_seo_filters.navigateToSeoPagesList();
+        $("a[href$='android-2.2-froyo-samsung/']").click();
+        shiftBrowserTab(1);
+        String urlOfSeoPage = WebDriverRunner.getWebDriver().getCurrentUrl();   //получили ссылку на SEO-страницу
+        shiftBrowserTab(0);
+
+        //Настраиваем XML-карту сайта
+        SitemapSettings sitemapSettings = csCartSettings.navigateToSitemapSettings();
+        sitemapSettings.tab_Settings.click();
+        sitemapSettings.tab_XMLSitemap.click();
+        if(!sitemapSettings.setting_EnableXMLSitemap.isSelected()){
+            sitemapSettings.setting_EnableXMLSitemap.click();   }
+        if(!sitemapSettings.setting_CategoriesSettings_IncludeToSitemap.isSelected()){
+            sitemapSettings.setting_CategoriesSettings_IncludeToSitemap.click();
+        }
+        csCartSettings.button_Save.click();
+
+        //Работаем с выгрузкой
+        csCartSettings.navigateToSitemapGenerating();
+        sitemapSettings.clickButton_GenerateSitemap();
+        $("a[href*='sitemap.xml']").click();
+        shiftBrowserTab(2);
+        String urlForXMLCategories = sitemapSettings.splitLinkMethod(3);
+        Selenide.executeJavaScript("window.open('" + urlForXMLCategories + "');");
+        shiftBrowserTab(3);
+        //Проверяем, что ссылка на SEO-страницу для фильтров присутствует
+        SoftAssert softAssert = new SoftAssert();
+        softAssert.assertTrue($(".pretty-print").has(Condition.text(urlOfSeoPage)),
+                "There is no link to the SEO page for filters in the sitemap 'categories2.xml'!");
     }
 }
